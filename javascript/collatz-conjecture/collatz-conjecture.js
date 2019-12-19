b5baa0576312acc6f08ba96b3e27805ac0fd295f
Adam Morris <amorris2003@gmail.com>
2019-12-20 09:43:42 +1000

    Improved collatz-conjecture

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -1,14 +1,6 @@
-export const steps = (n) => {
-    let steps = 0;
-
-    if (n <= 0) {
+export const steps = (n, nSteps = 0) => {
+    if (n <= 0)
         throw new Error('Only positive numbers are allowed');
-    }
 
-    while (n != 1) {
-        n = n % 2 ? 3*n+1 : n/2;
-        steps++;
-    }
-
-    return steps;
+    return (n == 1) ? nSteps : steps(n % 2 ? 3*n+1 : n/2, nSteps + 1);
 };
