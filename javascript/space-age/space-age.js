908ebd0ad6f4895a8a282bf5f47ff10a1dd59dc9
Adam Morris <amorris2003@gmail.com>
2019-12-20 15:11:26 +1000

    space-age passing

---
+++ javascript/space-age/space-age.js
@@ -0,0 +1,12 @@
+export const age = (planet, seconds) => Math.round(seconds / 315576 / PLANETS[planet])/100;
+
+export const PLANETS = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1.0,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+}
