83b74f767a8710d1fad94a1c83a1a690828c5db5
Adam Morris <amorris2003@gmail.com>
2019-12-21 15:18:11 +1000

    hamming improvement

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,14 +1,10 @@
 export const compute = (seqA, seqB) => {
-  if (seqA == '') {
-    if (seqB == '')
-        return 0;
-    throw new Error('left strand must not be empty');
-  } else if (seqB == '') {
-    throw new Error('right strand must not be empty');
-  }
+  if (seqA.length !== seqB.length) {
+    if (!seqA.length) throw new Error('left strand must not be empty');
+    if (!seqB.length) throw new Error('right strand must not be empty');
 
-  if (seqA.length !== seqB.length)
     throw new Error('left and right strands must be of equal length');
+  }
 
   return seqA.split('').filter((x, i) => x !== seqB[i]).length;
 };
