af9914f484e628b0b51d1b0bf047368e6ebb1b12
Adam Morris <amorris2003@gmail.com>
2019-12-22 07:10:07 +1000

    raindrops strict equality

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -6,9 +6,9 @@
 export const convert = (x) => {
   let str = '';
 
-  if (x % 3 == 0) str += 'Pling';
-  if (x % 5 == 0) str += 'Plang';
-  if (x % 7 == 0) str += 'Plong';
+  if (x % 3 === 0) str += 'Pling';
+  if (x % 5 === 0) str += 'Plang';
+  if (x % 7 === 0) str += 'Plong';
 
   return str || x.toString();
 };
