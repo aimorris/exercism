6b7b8135f299437444da901376478542e867858c
Adam Morris <amorris2003@gmail.com>
2019-12-22 07:36:34 +1000

    resistor-color-duo finished

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,3 @@
+export const decodedValue = x => Number('' + COLORS.indexOf(x[0]) + COLORS.indexOf(x[1]))
+
+export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
