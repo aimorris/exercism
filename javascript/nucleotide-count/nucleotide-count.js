d38b1d9189ae51d1f949c59cdc98140c6216e0ae
Adam Morris <amorris2003@gmail.com>
2019-12-21 18:39:27 +1000

    nucleotide-count passing

---
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -0,0 +1,11 @@
+export class NucleotideCounts {
+  static parse(sequence) {
+    let seq = sequence.toUpperCase().split('');
+    if (seq.filter(x => ['A', 'C', 'G', 'T'].includes(x)).length !== seq.length) throw new Error('Invalid nucleotide in strand');
+
+    return count(seq, 'A') + ' ' + count(seq, 'C') + ' ' + count(seq, 'G') + ' ' + count(seq, 'T');
+
+  }
+}
+
+export const count = (array, x) => array.filter((val, i) => array[i] == x).length;
