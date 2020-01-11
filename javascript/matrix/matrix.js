d649f0387558ef5854c50faf58f3c9b1a4c6c3e5
Adam Morris <amorris2003@gmail.com>
2020-01-11 16:27:24 +1000

    remove comment

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Matrix' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Matrix {
   constructor(string) {
     this.matrixArray = string.split('\n').map(x => x.split(' ').map(x => Number(x)));
