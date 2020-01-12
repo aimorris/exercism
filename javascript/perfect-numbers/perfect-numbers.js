26ca87258b8864dd7a6f7a9a03c9064fa1ac692a
Adam Morris <amorris2003@gmail.com>
2020-01-13 06:59:57 +1000

    perfect-numbers passing

---
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -0,0 +1,32 @@
+export const classify = (num) => {
+
+  if (num < 1)
+    throw new Error('Classification is only possible for natural numbers.');
+
+  let classification = sumOfArray(factorise(num));
+
+  if (classification > num) {
+    return 'abundant';
+  } else if (classification < num) {
+    return 'deficient';
+  } else {
+    return 'perfect';
+  }
+}
+
+export const sumOfArray = (arr) => {
+  return arr.reduce( (acc, cur) => {
+      return acc + cur
+    }, 0);
+}
+
+export const factorise = (num) => {
+  let out = [];
+
+  for (let i = 0; i < num; i++) {
+    if (num % i === 0)
+      out.push(i);
+  }
+
+  return out;
+}
