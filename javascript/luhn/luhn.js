583a8b8fdf4b27687311b34acd9423fdb1cf2265
Adam Morris <amorris2003@gmail.com>
2020-01-12 18:15:19 +1000

    all one function

--- javascript/luhn/luhn.js
+++ javascript/luhn/luhn.js
@@ -5,12 +5,8 @@ export const valid = number => {
   if (num.length <= 1)
     return false;
 
-  return sumDigits('' + num.split('').reverse().reduce((acc, cur, index) => {
+  return ('' + num.split('').reverse().reduce((acc, cur, index) => {
       let num = index & 1 ? cur * 2 : cur;
       return (num > 9 ? acc + num - 9 : acc + num);
-    })) % 10 === 0
+    })).split('').map(Number).reduce((x, y) => x + y) % 10 == 0
 };
-
-export const sumDigits = n => {
-  return n.split('').map(Number).reduce((x, y) => x + y);
-}
