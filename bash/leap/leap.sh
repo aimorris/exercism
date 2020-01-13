ccbab1035f2c94acb00ac641ca75fa4b8fda2a0b
Adam Morris <amorris2003@gmail.com>
2020-01-14 06:33:01 +1000

    leap fix mistakes

--- bash/leap/leap.sh
+++ bash/leap/leap.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 main () {
-if [[ $1 =~ [^0-9]+ ]] || [ $# -ne 1 ]; then
+if [[ $1 =~ [^0-9] ]] || [[ $# -ne 1 ]]; then
   echo 'Usage: leap.sh <year>';
   exit 1;
-elif [[ $(($1 % 400)) -eq 0 ]]; then
+elif (( $1 % 400 == 0)); then
   echo 'true';
-elif [[ $(($1 % 100)) -eq 0 ]]; then
+elif (( $1 % 100 == 0)); then
   echo 'false';
-elif [[ $(($1 % 4)) -eq 0 ]]; then
+elif (( $1 % 4 == 0)); then
   echo 'true';
 else
   echo 'false';
