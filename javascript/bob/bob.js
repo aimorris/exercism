5a41c031c1582b5315077730e54448cb5947388c
Adam Morris <amorris2003@gmail.com>
2020-01-13 10:53:32 +1000

    bob remove if-else

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,14 +1,9 @@
 export const hey = function(prompt) {
    if (prompt.toUpperCase() == prompt && /[a-zA-Z]/.test(prompt)) {
-        if (prompt.replace(/\s/g, '').slice(-1) == '?') {
-            return 'Calm down, I know what I\'m doing!'
-        }
+        if (prompt.replace(/\s/g, '').slice(-1) == '?') return 'Calm down, I know what I\'m doing!';
      return 'Whoa, chill out!';
-   } else if (!/\S/g.test(prompt)) {
-     return 'Fine. Be that way!';
-   } else if (prompt.replace(/\s/g, '').slice(-1) == '?') {
-     return 'Sure.';
-   } else {
-     return 'Whatever.';
    }
+   if (!/\S/g.test(prompt)) return 'Fine. Be that way!';
+   if (prompt.replace(/\s/g, '').slice(-1) == '?') return 'Sure.';
+   return 'Whatever.';
  }
