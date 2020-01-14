7c75e6fe7552abe4d6d91936cc4205a495bf60cd
Adam Morris <amorris2003@gmail.com>
2020-01-15 06:29:30 +1000

    refactored luhn

--- javascript/luhn/luhn.js
+++ javascript/luhn/luhn.js
@@ -5,8 +5,14 @@ export const valid = number => {
   if (num.length <= 1)
     return false;
 
-  return ('' + num.split('').reverse().reduce((acc, cur, index) => {
+  return ([...`${reducedNumber(num)}`].map(Number).reduce((x, y) => x + y) % 10 == 0)
+};
+
+export const reducedNumber = num => {
+  return [...num]
+  .reverse()
+  .reduce((acc, cur, index) => {
       let num = index & 1 ? cur * 2 : cur;
       return (num > 9 ? acc + num - 9 : acc + num);
-    })).split('').map(Number).reduce((x, y) => x + y) % 10 == 0
-};
+    }, 0)
+}
