7172d7163d0ab72067e37b9727c37cf4d674acc5
Adam Morris <amorris2003@gmail.com>
2020-01-14 14:52:00 +1000

    performance improvements

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -4,4 +4,4 @@ isDiv :: Integer -> Integer -> Bool
 isDiv x y = rem x y == 0
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = (isDiv year 4 && not (isDiv year 100)) || isDiv year 400
+isLeapYear year = isDiv year 4 && (not (isDiv year 100) || isDiv year 400)
