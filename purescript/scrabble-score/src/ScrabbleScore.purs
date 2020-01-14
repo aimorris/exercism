61e7a9aa79374e91b730a688e30e96cb105d0974
Adam Morris <amorris2003@gmail.com>
2020-01-15 07:43:49 +1000

    scrabble-score purs

---
+++ purescript/scrabble-score/src/ScrabbleScore.purs
@@ -0,0 +1,39 @@
+module ScrabbleScore (scoreWord) where
+
+import Data.String.CodeUnits (toCharArray)
+import Data.Foldable (sum)
+import Prelude (map, (<<<))
+import Data.String (toUpper)
+
+scrabbleValues :: Char -> Int
+scrabbleValues x = case x of
+  'A' -> 1
+  'B' -> 3
+  'C' -> 3
+  'D' -> 2
+  'E' -> 1
+  'F' -> 4
+  'G' -> 2
+  'H' -> 4
+  'I' -> 1
+  'J' -> 8
+  'K' -> 5
+  'L' -> 1
+  'M' -> 3
+  'N' -> 1
+  'O' -> 1
+  'P' -> 3
+  'Q' -> 10
+  'R' -> 1
+  'S' -> 1
+  'T' -> 1
+  'U' -> 1
+  'V' -> 4
+  'W' -> 4
+  'X' -> 8
+  'Y' -> 4
+  'Z' -> 10
+  _ -> 0
+
+scoreWord :: String -> Int
+scoreWord = sum <<< map scrabbleValues <<< toCharArray <<< toUpper
