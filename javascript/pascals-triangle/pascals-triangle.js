c55cd3e156d02525f74239f91b9442986bf903fb
Adam Morris <amorris2003@gmail.com>
2020-01-15 06:16:15 +1000

    pascals-triangle refactor

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -8,20 +8,15 @@ export class Triangle {
   }
 
   get rows() {
-    let out = [];
+    let out = [[1]];
 
-    if (this._rows === 1)
-      out.push([1])
+    for (let i = 1; i < this._rows; i++) {
+      let row = [1];
 
-    if (this._rows > 1)
-      out.push([1], [1, 1])
-
-    for (let i = 2; i < this._rows; i++) {
-      let row = [];
-      row.push(1);
       for (let j = 1; j < i; j++) {
         row.push(out[i-1][j-1] + out[i-1][j]);
       }
+
       row.push(1);
       out.push(row);
     }
