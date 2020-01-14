770fbcca8e3acbc3bb91e32ff1d4342ce3edac14
Adam Morris <amorris2003@gmail.com>
2020-01-15 08:59:04 +1000

    triangle passing

---
+++ purescript/triangle/src/Triangle.purs
@@ -0,0 +1,27 @@
+module Triangle (Triangle(Equilateral, Isosceles, Scalene), triangleKind) where
+
+import Data.Either (Either (..))
+import Data.Set as Set
+import Prelude
+
+data Triangle = Equilateral
+              | Isosceles
+              | Scalene
+derive instance eqTriangle :: Eq Triangle
+
+instance showTriangle :: Show Triangle
+  where show Equilateral = "Equilateral"
+        show Isosceles = "Isosceles"
+        show Scalene = "Scalene"
+
+triangleKind :: Int -> Int -> Int -> Either String Triangle
+triangleKind s1 s2 s3 = findKind s1 s2 s3
+  where triangleSet = Set.fromFoldable [s1, s2, s3]
+        findKind :: Int -> Int -> Int -> Either String Triangle
+        findKind x y z
+          | x < 1 || y < 1 || z < 1 = Left "Invalid lengths"
+          | x + y < z || y + z < x || z + x < y = Left "Violates inequality"
+          | Set.size triangleSet == 3 = Right Scalene
+          | Set.size triangleSet == 2 = Right Isosceles
+          | Set.size triangleSet == 1 = Right Equilateral
+          | otherwise = Left "no"
