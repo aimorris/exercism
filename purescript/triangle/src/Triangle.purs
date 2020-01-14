db7ad81aa130bc8a8af93f909fbcb6f431121a80
Adam Morris <amorris2003@gmail.com>
2020-01-15 09:01:28 +1000

    perf improv

--- purescript/triangle/src/Triangle.purs
+++ purescript/triangle/src/Triangle.purs
@@ -21,7 +21,6 @@ triangleKind s1 s2 s3 = findKind s1 s2 s3
         findKind x y z
           | x < 1 || y < 1 || z < 1 = Left "Invalid lengths"
           | x + y < z || y + z < x || z + x < y = Left "Violates inequality"
-          | Set.size triangleSet == 3 = Right Scalene
-          | Set.size triangleSet == 2 = Right Isosceles
           | Set.size triangleSet == 1 = Right Equilateral
-          | otherwise = Left "no"
+          | Set.size triangleSet == 2 = Right Isosceles
+          | otherwise = Right Scalene
