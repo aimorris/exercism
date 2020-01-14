d6288d2968226a5b582ccbe1e513ddf073cfbead
Adam Morris <amorris2003@gmail.com>
2020-01-14 17:24:24 +1000

    rna-transcription pass

---
+++ purescript/rna-transcription/src/RNATranscription.purs
@@ -0,0 +1,17 @@
+module RNATranscription (toRNA) where
+
+import Data.String.CodeUnits as String
+import Data.Maybe (Maybe (..))
+import Data.Traversable (traverse)
+import Prelude
+
+nucleotide :: Char -> Maybe Char
+nucleotide x = case x of
+  'G' -> Just 'C'
+  'C' -> Just 'G'
+  'T' -> Just 'A'
+  'A' -> Just 'U'
+  _ -> Nothing
+
+toRNA :: String -> Maybe String
+toRNA x = map String.fromCharArray $ traverse nucleotide $ String.toCharArray x
