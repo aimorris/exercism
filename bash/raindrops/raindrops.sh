69f31fadc4837936726a81fc5508ba8b87da71ea
Adam Morris <amorris2003@gmail.com>
2020-01-15 11:02:05 +1000

    raindrops bash

---
+++ bash/raindrops/raindrops.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+main () {
+out="";
+
+if (( $1 % 3 == 0 )); then
+  out+="Pling";
+fi
+
+if (( $1 % 5 == 0 )); then
+  out+="Plang";
+fi
+
+if (( $1 % 7 == 0 )); then
+  out+="Plong";
+fi
+
+if [[ $out == "" ]]; then
+  out+=$1;
+fi
+
+echo $out;
+}
+
+main "$@"
