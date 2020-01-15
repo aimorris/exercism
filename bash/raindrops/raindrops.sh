f21ba0d34f4b111cf251ec5a08e3284f42928e11
Adam Morris <amorris2003@gmail.com>
2020-01-15 11:03:31 +1000

    bash cleanup

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -3,21 +3,11 @@
 main () {
 out="";
 
-if (( $1 % 3 == 0 )); then
-  out+="Pling";
-fi
+if (( $1 % 3 == 0 )); then out+="Pling"; fi
+if (( $1 % 5 == 0 )); then out+="Plang"; fi
+if (( $1 % 7 == 0 )); then out+="Plong"; fi
 
-if (( $1 % 5 == 0 )); then
-  out+="Plang";
-fi
-
-if (( $1 % 7 == 0 )); then
-  out+="Plong";
-fi
-
-if [[ $out == "" ]]; then
-  out+=$1;
-fi
+if [[ $out == "" ]]; then out+=$1; fi
 
 echo $out;
 }
