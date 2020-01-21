2047d69b9fec49c73047629d14629e3c27c3356c
Adam Morris <amorris2003@gmail.com>
2020-01-22 05:56:42 +1000

    refactor

--- haskell/twelve-days/src/TwelveDays.hs
+++ haskell/twelve-days/src/TwelveDays.hs
@@ -1,26 +1,26 @@
 module TwelveDays (recite) where
 
 recite :: Int -> Int -> [String]
-recite start stop = map (lyrics) [start..stop]
+recite start stop = map lyrics [start..stop]
 
 lyrics :: Int -> String
 lyrics x = "On the " ++ ordinal x ++ " day of Christmas my true love gave to me: " ++ gifts x
 
 gifts :: Int -> String
-gifts x = case x of
-  1 -> "a Partridge in a Pear Tree."
-  2 -> "two Turtle Doves, and " ++ gifts (x - 1)
-  3 -> "three French Hens, " ++ gifts (x - 1)
-  4 -> "four Calling Birds, " ++ gifts (x - 1)
-  5 -> "five Gold Rings, " ++ gifts (x - 1)
-  6 -> "six Geese-a-Laying, " ++ gifts (x - 1)
-  7 -> "seven Swans-a-Swimming, " ++ gifts (x - 1)
-  8 -> "eight Maids-a-Milking, " ++ gifts (x - 1)
-  9 -> "nine Ladies Dancing, " ++ gifts (x - 1)
-  10 -> "ten Lords-a-Leaping, " ++ gifts (x - 1)
-  11 -> "eleven Pipers Piping, " ++ gifts (x - 1)
-  12 -> "twelve Drummers Drumming, " ++ gifts (x - 1)
-  _ -> error "Number not between 1 and 12"
+gifts x = concatMap part [x, x-1 .. 1]
+  where part 1 = "a Partridge in a Pear Tree."
+        part 2 = "two Turtle Doves, and "
+        part 3 = "three French Hens, "
+        part 4 = "four Calling Birds, "
+        part 5 = "five Gold Rings, "
+        part 6 = "six Geese-a-Laying, "
+        part 7 = "seven Swans-a-Swimming, "
+        part 8 = "eight Maids-a-Milking, "
+        part 9 = "nine Ladies Dancing, "
+        part 10 = "ten Lords-a-Leaping, "
+        part 11 = "eleven Pipers Piping, "
+        part 12 = "twelve Drummers Drumming, "
+        part _ = error "Number not between 1 and 12"
 
 ordinal :: Int -> String
 ordinal x = case x of
