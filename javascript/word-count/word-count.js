e61a7e02f8836b97b3cfd27fa69e9990cea736f8
Adam Morris <amorris2003@gmail.com>
2020-02-01 11:06:15 +1000

    one line

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,9 +1,4 @@
-export const countWords = str =>
-  str
-  .toLowerCase()
-  .match(/\w+('\w+)?/g)
-  .reduce(
-    (obj, word) => {
-      obj[word] = obj[word] + 1 || 1;
-      return obj
-    }, {});
+export const countWords = str => str.toLowerCase().match(/\w+('\w+)?/g).reduce((obj, word) => {
+  obj[word] = obj[word] + 1 || 1;
+  return obj
+}, {});
