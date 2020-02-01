07ed1c1d988f1c0c0aaec9d28c32f01e9b6c24cd
Adam Morris <amorris2003@gmail.com>
2020-02-01 11:04:05 +1000

    one line

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,16 +1,11 @@
 export class Triangle {
-  constructor(a, b, c) {
-    this.sides = [a, b, c].sort((a, b) => a - b);
-  }
+  constructor(a, b, c) { this.sides = [a, b, c].sort((a, b) => a - b) }
 
   kind() {
     const sides = this.sides;
 
-  	if (sides[0] + sides[1] < sides[2])
-  		throw new Error("violates triangle inequality");
-  	
-    if (sides[0] <= 0)
-  		throw new Error("triangle with invalid side length");
+  	if (sides[0] + sides[1] < sides[2]) throw new Error("violates triangle inequality");
+    if (sides[0] <= 0) throw new Error("triangle with invalid side length");
 
     return TRIANGLE_TYPES[new Set(this.sides).size];
   }
