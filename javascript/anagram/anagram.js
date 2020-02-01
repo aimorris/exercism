135e1bdfadd528a4899e30b8960bff7c25d39764
Adam Morris <amorris2003@gmail.com>
2020-02-01 10:55:38 +1000

    one line

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,6 +1,3 @@
-export const findAnagrams = (word, list) => list.filter(x =>
-  (word.toLowerCase() !== x.toLowerCase()) &&
-  (sorted(x) == sorted(word))
-)
+export const findAnagrams = (word, list) => list.filter(x => (word.toLowerCase() !== x.toLowerCase()) && (sorted(x) == sorted(word)))
 
 export const sorted = (x) => x.toLowerCase().split('').sort().join('')
