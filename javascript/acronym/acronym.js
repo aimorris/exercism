b4167dd0404c1ed73d41bf7c7658b5093b20677f
Adam Morris <amorris2003@gmail.com>
2020-02-01 10:52:18 +1000

    one lines

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -1,5 +1,1 @@
-export const parse = s => s
-  .toUpperCase()
-  .match(/[A-Z']+/g)
-  .map(x => x[0])
-  .join('')
+export const parse = s => s.toUpperCase().match(/[A-Z']+/g).map(x => x[0]).join('')
