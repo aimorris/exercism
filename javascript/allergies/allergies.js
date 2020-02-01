8718e8a75f2882613a80518db55a6569a8c1a086
Adam Morris <amorris2003@gmail.com>
2020-02-01 10:55:20 +1000

    one line

--- javascript/allergies/allergies.js
+++ javascript/allergies/allergies.js
@@ -1,15 +1,9 @@
 export class Allergies {
-  constructor(number) {
-    this.allergies = number.toString(2).split('').reverse();
-  }
+  constructor(number) { this.allergies = number.toString(2).split('').reverse() }
+  
+  list() { return POSSIBLE_ALLERGIES.filter((x, i) => this.allergies[i] === '1') }
 
-  list() {
-    return POSSIBLE_ALLERGIES.filter((x, i) => this.allergies[i] === '1');
-  }
-
-  allergicTo(x) {
-    return this.list().includes(x);
-  }
+  allergicTo(x) { return this.list().includes(x) }
 }
 
 export const POSSIBLE_ALLERGIES = ['eggs', 'peanuts', 'shellfish', 'strawberries', 'tomatoes', 'chocolate', 'pollen', 'cats'];
