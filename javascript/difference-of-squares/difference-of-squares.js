a3676a3aede6f46a6f4832a5d239054078d3c3a4
Adam Morris <amorris2003@gmail.com>
2020-02-01 10:57:47 +1000

    one line

--- javascript/difference-of-squares/difference-of-squares.js
+++ javascript/difference-of-squares/difference-of-squares.js
@@ -4,30 +4,14 @@ export class Squares {
 
     this.list = [];
 
-    for (let i = 1; i <= this.n; i++) {
-      this.list.push(i);
-    }
+    for (let i = 1; i <= this.n; i++) { this.list.push(i) }
   }
 
-  get sumOfSquares() {
-    return sumOfArray(
-      this.list.map(
-        x => Math.pow(x, 2)
-      )
-    );
-  }
+  get sumOfSquares() { return sumOfArray(this.list.map(x => Math.pow(x, 2))) }
 
-  get squareOfSum() {
-    return Math.pow(sumOfArray(this.list), 2);
-  }
+  get squareOfSum() { return Math.pow(sumOfArray(this.list), 2) }
 
-  get difference() {
-    return this.squareOfSum - this.sumOfSquares;
-  }
+  get difference() { return this.squareOfSum - this.sumOfSquares }
 }
 
-export const sumOfArray = (arr) => {
-  return arr.reduce( (acc, cur) => {
-      return acc + cur
-    }, 0);
-}
+export const sumOfArray = (arr) => { return arr.reduce((acc, cur) => { return acc + cur }, 0) }
