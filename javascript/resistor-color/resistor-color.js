8b67981e1bc1898854d3d7e306a2d3b8434cd8d8
Adam Morris <amorris2003@gmail.com>
2020-02-01 11:03:03 +1000

    one line

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,5 +1,3 @@
-export const colorCode = (code) => {
-  return COLORS.indexOf(code);
-};
+export const colorCode = (code) => COLORS.indexOf(code)
 
-export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"]
