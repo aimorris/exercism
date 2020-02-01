68df6d414d9fccf021bfc4236f4c46275d84d788
Adam Morris <amorris2003@gmail.com>
2020-02-01 10:56:39 +1000

    one line

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -1,6 +1,4 @@
 export const steps = (n, nSteps = 0) => {
-    if (n <= 0)
-        throw new Error('Only positive numbers are allowed');
-
-    return (n == 1) ? nSteps : steps(n % 2 ? 3*n+1 : n/2, nSteps + 1);
+    if (n <= 0) throw new Error('Only positive numbers are allowed')
+    return (n == 1) ? nSteps : steps(n % 2 ? 3*n+1 : n/2, nSteps + 1)
 };
