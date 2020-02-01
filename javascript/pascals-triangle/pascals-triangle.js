755d1586cade2cf1ae977177d264f22ee3e5ef50
Adam Morris <amorris2003@gmail.com>
2020-02-01 11:01:27 +1000

    one line

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,21 +1,15 @@
 export class Triangle {
-  constructor(rows) {
-    this._rows = rows;
-  }
+  constructor(rows) { this._rows = rows }
 
-  get lastRow() {
-    return this.rows.slice(-1)[0];
-  }
+  get lastRow() { return this.rows.slice(-1)[0] }
 
   get rows() {
     let out = [[1]];
 
     for (let i = 1; i < this._rows; i++) {
       let row = [1];
-
-      for (let j = 1; j < i; j++) {
-        row.push(out[i-1][j-1] + out[i-1][j]);
-      }
+      
+      for (let j = 1; j < i; j++) { row.push(out[i-1][j-1] + out[i-1][j]) }
 
       row.push(1);
       out.push(row);
