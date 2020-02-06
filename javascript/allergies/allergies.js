f0e670845e2fa87de50aed5fa2d293fa102553d8
Adam Morris <amorris2003@gmail.com>
2020-02-06 17:39:01 +1000

    less char

--- javascript/allergies/allergies.js
+++ javascript/allergies/allergies.js
@@ -1,9 +1,7 @@
 export class Allergies {
-  constructor(number) { this.allergies = number.toString(2).split('').reverse() }
-  
-  list() { return POSSIBLE_ALLERGIES.filter((x, i) => this.allergies[i] === '1') }
-
+  constructor(number) { this.allergies = [...number.toString(2)].reverse() }
+  list() { return ALLERGIES.filter((_, i) => this.allergies[i] == '1') }
   allergicTo(x) { return this.list().includes(x) }
 }
 
-export const POSSIBLE_ALLERGIES = ['eggs', 'peanuts', 'shellfish', 'strawberries', 'tomatoes', 'chocolate', 'pollen', 'cats'];
+export const ALLERGIES = ['eggs', 'peanuts', 'shellfish', 'strawberries', 'tomatoes', 'chocolate', 'pollen', 'cats'];
